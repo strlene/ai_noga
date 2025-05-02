aca3b30a5dc3a600c8fa636c03cdce84c0e9baa2
reihannudin24 <reihannudin24@gmail.com>
2025-05-02 14:45:08 +0700

    Update Commit : Define Url for Ocr Ingredients

--- api/app.py
+++ api/app.py
@@ -4,6 +4,9 @@ from fastapi.requests import Request
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi import FastAPI, HTTPException
 
+from handler.ocr_handler import analyze_images
+from schema.schema import AnalyzeResponse, AnalyzeRequest
+
 SOURCES = {
     "ingredients":  "http://localhost:3000/uploads/composition/composition-4tjcofu8wx.jpeg",
 }
@@ -36,3 +39,16 @@ app.add_middleware(
 @app.get("/")
 def read_root():
     return {"message": "Hello World"}
+
+
+@app.post("/ocr-ingredients", response_model=AnalyzeResponse)
+def analyze_default(req: AnalyzeRequest):
+    try:
+        sources = {
+            "ingredients": str(req.composition),
+        }
+        return analyze_images(req.session_id, sources)
+    except HTTPException as e:
+        raise e
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
