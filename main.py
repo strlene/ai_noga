af749461a3f62be9c99bc37cf17d6a99f45c1899
reihannudin24 <reihannudin24@gmail.com>
2025-05-02 14:09:49 +0700

    Initialize Commit For Ai In Ingredients

---
+++ main.py
@@ -0,0 +1,10 @@
+
+print("Hello world")
+
+#
+# if __name__ == "__main__":
+#     import uvicorn
+#     import os
+#
+#     port = int(os.environ.get("PORT", 8080))  # ✅ default 8080
+#     uvicorn.run("main:app", host="0.0.0.0", port=port)
